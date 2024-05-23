1c260b7efe26bbb248be9fefd875246c25cdff84
CorrM <islamnofl.official@gmail.com>
2024-05-23 12:21:05 +0300

    Check and create 'sddm.conf' example

--- fix_sddm_resolution.sh
+++ fix_sddm_resolution.sh
@@ -8,7 +8,7 @@ fi
 
 # Check if current session is x11
 # https://www.reddit.com/r/archlinux/comments/143b6we/how_to_display_login_screen_sddm_on_a_single/
-if [ "$XDG_SESSION_TYPE" == "x11" ]; then
+if [ "$XDG_SESSION_TYPE" != "x11" ]; then
     echo "This script is designed for use within an X11 session environment."
     exit 1
 fi
@@ -22,8 +22,24 @@ if [[ $(systemctl is-active sddm.service) != "active" ]]; then
     fi
 fi
 
+# Check if /etc/sddm.conf exists and is not empty
+# https://www.reddit.com/r/archlinux/comments/52ehk8/comment/d7jl2gz/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+if [[ ! -s /etc/sddm.conf ]]; then
+    read -p "/etc/sddm.conf does not exist or is empty. Do you want to create example config? (y/n): " choice
+    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+        sddm --example-config | sudo tee /etc/sddm.conf
+        if [[ $? -ne 0 ]]; then
+            echo "Failed to create /etc/sddm.conf. Please create it manually."
+            exit 1
+        fi
+    else
+        echo "Please create /etc/sddm.conf to proceed."
+        exit 1
+    fi
+fi
+
 # Get the output of the xrandr command
-xrandr_output=$(xrandr)
+xrandr_output=$(/usr/bin/xrandr)
 
 # Parse connected monitors and their resolutions with refresh rates
 connected_monitors=()
@@ -69,13 +85,14 @@ select res_rate in "${resolutions[@]}"; do
 done
 
 # Prepare the xrandr command to set the primary monitor and resolution
-xrandr_command="xrandr --output \"$primary_monitor\" --primary --mode ${res_rate%@*} --rate ${res_rate#*@}"
+# https://github.com/sddm/sddm/issues/958#issuecomment-353714073
+xrandr_command="/usr/bin/xrandr --output \"$primary_monitor\" --primary --mode ${res_rate%@*} --rate ${res_rate#*@}"
 
 # Prepare the commands to turn off other monitors
 turn_off_commands=""
 for monitor in "${connected_monitors[@]}"; do
     if [[ "$monitor" != "$primary_monitor" ]]; then
-        turn_off_commands+="xrandr --output \"$monitor\" --off\n"
+        turn_off_commands+="/usr/bin/xrandr --output \"$monitor\" --off\n"
     fi
 done
 
@@ -91,7 +108,7 @@ echo "A backup of the original Xsetup file has been created at $backup_file"
 awk -v cmd="$xrandr_command" -v turn_off="$turn_off_commands" '
     BEGIN {inserted=0}
     /^#/ {print; next}
-    !inserted {print cmd; print turn_off; inserted=1}
+    !inserted {print cmd; printf turn_off; inserted=1}
     {print}
 ' "$xsetup_file" > "$xsetup_file.new"
 
