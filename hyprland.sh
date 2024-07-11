32070649b465f3efe11ee32d1f31949e3ac28f57
CorrM <islamnofl.official@gmail.com>
2024-07-11 03:33:29 +0300

    Remove hyprland.sh

--- hyprland.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yay -Syu otf-font-awesome ttf-arimo-nerd noto-fonts hyprland-git hyprpaper-git xdg-desktop-portal-hyprland-git rofi-wayland alacritty-git hypridle-git hyprlock-git waybar wlogout nwg-look
-
-ln -s "$(pwd)/alacritty" ~/.config
-ln -s "$(pwd)/hypr" ~/.config
-ln -s "$(pwd)/waybar" ~/.config
