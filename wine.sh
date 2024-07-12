39b18f0bea928f9a93de59a92c07118a419914f8
CorrM <islamnofl.official@gmail.com>
2024-07-12 23:34:13 +0300

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -9,15 +9,42 @@ if ! command -v wine &> /dev/null; then
   exit 1
 fi
 
-# Check if wine is installed
+# Check if winetricks is installed
 if ! command -v winetricks &> /dev/null; then
   echo "winetricks could not be found. Please install winetricks."
   exit 1
 fi
 
-options=("Install dotnet48 and d3dcompiler_47" "Delete default prefix" "Quit")
+# Check if yay is installed
+if ! command -v yay --version &> /dev/null; then
+  echo "yay could not be found. Please install yay."
+  exit 1
+fi
+
+options=("Install wine-tkg" "Install dotnet48 and d3dcompiler_47" "Delete default prefix" "Quit")
 select opt in "${options[@]}"; do
   case $opt in
+    "Install wine-tkg")
+      echo "Install wine-tkg..."
+
+      # Make cache folder
+      if [ -d "wine-cache" ]; then
+        rm -rf wine-cache
+      fi
+
+      mkdir wine-cache
+      cd wine-cache
+
+      # Download wine
+      wget -P ./ --no-check-certificate --no-proxy https://nightly.link/Frogging-Family/wine-tkg-git/workflows/wine-arch/master/wine-tkg-build.zip
+
+      # Install
+      unzip wine-tkg-build.zip
+      yay -U wine-tkg-*.pkg.tar.zst
+
+      break
+    ;;
+
     "Install dotnet48 and d3dcompiler_47")
         echo "Install dotnet48 and d3dcompiler_47..."
         winetricks -q dotnet48 d3dcompiler_47
